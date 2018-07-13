f86460caaf4c00cd8879347f23154d95a324cf51
Rhi88 <rhizdo_nop@hotmail.com>
2018-07-13 12:50:19 -0500

    firebase update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,17 @@ export class AppComponent {
   constructor(db: AngularFirestore){
     this.usuarios =db.collection('use').valueChanges();
   }
+  ngOnInit() {
+    this.createIDs(this.usuarios);
+  }
+  createIDs(usuarios): void {
+    for(let usuario of usuarios){
+      const   FIRST_TWO_CHARACTERS=usuario.primer_apellido.substring(1,3);
+      const   SECOND_TWO_CHARACTERS=usuario.segundo_apellido.substring(1,3);
+      const   THIRD_TWO_CHARACTERS=usuario.nombre.substring(1,3);
+      const   FIRST_PART=FIRST_TWO_CHARACTERS+SECOND_TWO_CHARACTERS+THIRD_TWO_CHARACTERS;
+      usuario.string_id =FIRST_PART.toUpperCase();
+      usuario.update(usuario);
+    }
+  }
 }
